21b1769171a15db998ff2fe7d1e10d66c7dca964
santiago rodriguez <9152404+santiagorodriguez-dev@users.noreply.github.com>
2025-02-19 23:55:07 +0100

    post

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import sys
+sys.path.append("../")
+import os
+from dotenv import load_dotenv # type: ignore
+load_dotenv()
 
 app = FastAPI()
 
-@app.get("/load_data")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+class DataModel(BaseModel):
+    code: str
+
+
+@app.post("/load_data")
+async def load_data(data: DataModel):
+    try:
+        if data.code == os.getenv("secret"):
+            return {"message": "Data loaded successfully"}
+        else:
+            return HTTPException(status_code=401, detail="Unauthorized", headers={"Authenticate": "Key not valid"})
+        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e), headers={"Error General": "Algo ha ido mal"})
